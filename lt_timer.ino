1baf74e04b6b6fa68fa7bd2bad3333ed680e35d6
Yuichi Sato <y_sato@nnn.ed.jp>
2019-12-18 10:08:15 +0900

    Update

--- lt_timer.ino
+++ lt_timer.ino
@@ -1,6 +1,8 @@
 #include <LedControl.h>
 
-#define PUSH_BUTTON 2
+#define START_BUTTON 2
+#define END_BUTTON 2
+
 #define SELECT_SWITCH 4
 
 LedControl lc = LedControl(12,11,10,1);
@@ -9,6 +11,7 @@ bool isLT;
 bool isCounting = false;
 bool isEnd = false;
 unsigned long beginTime, endTime;
+unsigned long t;
 
 void setup() {
   Serial.begin(9600);
@@ -17,20 +20,22 @@ void setup() {
   lc.setIntensity(0,8);
   lc.clearDisplay(0);
 
-  pinMode(PUSH_BUTTON, INPUT);
+  pinMode(START_BUTTON, INPUT_PULLUP);
+  pinMode(END_BUTTON, INPUT_PULLUP);
   pinMode(SELECT_SWITCH, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(PUSH_BUTTON), onReleaseButton, RISING);
+  attachInterrupt(digitalPinToInterrupt(START_BUTTON), onStartButton, FALLING);
+  attachInterrupt(digitalPinToInterrupt(END_BUTTON), onEndButton, FALLING);
 }
 
 void loop() {
-  unsigned long t = endTime - millis();
-
   if (endTime <= millis()) {
     isCounting = false;
   }
 
   if (isCounting) {
+    t = endTime - millis();
+
     if (isLT) {
       if ((2.5 * 60 * 1000) < t) {
       }
@@ -48,11 +53,70 @@ void loop() {
       }
     }
   }
+
+  display7Seg( t );
 }
 
-void onReleaseButton() {
+void display7Seg(unsigned long t) {
+  unsigned long min = t / 1000 / 60;
+  unsigned long sec = (t - (min * 60 * 1000)) / 1000;
+
+//  Serial.println (min);
+//  Serial.println (sec);
+
+  String strMin = String(min);
+  String strSec = String(sec);
+
+  bool isDp = sec & 0x01;
+
+  if(min < 10) {
+
+    lc.setChar(0, 0, ' ', false);
+    lc.setChar(0, 4, ' ', false);
+
+    if (min == 0) {
+      lc.setChar(0, 1, ' ', isDp);
+      lc.setChar(0, 5, ' ', isDp);
+    }
+    else {
+      lc.setDigit(0, 1, strMin.charAt(0) - '0', isDp);
+      lc.setDigit(0, 5, strMin.charAt(0) - '0', isDp);
+    }
+  }
+  else {
+    lc.setDigit(0, 0, strMin.charAt(0) - '0', false);
+    lc.setDigit(0, 1, strMin.charAt(1) - '0', isDp);
+    lc.setDigit(0, 4, strMin.charAt(0) - '0', false);
+    lc.setDigit(0, 5, strMin.charAt(1) - '0', isDp);
+  }
+
+  if(sec < 10) {
+    lc.setDigit(0, 2, 0,false);
+    lc.setDigit(0, 3, strSec.charAt(0) - '0', false);
+    lc.setDigit(0, 6, 0,false);
+    lc.setDigit(0, 7, strSec.charAt(0) - '0', false);
+  }
+  else {
+    lc.setDigit(0, 2, strSec.charAt(0) - '0', false);
+    lc.setDigit(0, 3, strSec.charAt(1) - '0', false);
+    lc.setDigit(0, 6, strSec.charAt(0) - '0', false);
+    lc.setDigit(0, 7, strSec.charAt(1) - '0', false);
+  }
+}
+
+void onStartButton() {
+  detachInterrupt(digitalPinToInterrupt(START_BUTTON));
+  Serial.println ("onStartButton");
+
   isLT = digitalRead(SELECT_SWITCH);
   beginTime = millis();
   endTime = isLT ? (5UL * 60UL * 1000UL + beginTime) : (15UL * 60UL * 1000UL + beginTime);
-  isCounting = !isCounting;
+  isCounting = true;
+}
+
+void onEndButton() {
+  Serial.println ("onEndButton");
+
+  attachInterrupt(digitalPinToInterrupt(START_BUTTON), onStartButton, FALLING);
+  isCounting = false;
 }
